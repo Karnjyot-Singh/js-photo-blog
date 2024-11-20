81f23ba37743a1b17b4d88c813d13b3a3aea34b9
Karnjyot <krnjyotsingh2004@email.com>
2024-11-20 19:32:30 +0100

    feat: prova push fine esercizio

--- js/script.js
+++ js/script.js
@@ -1,23 +1,55 @@
 // DATI
 const rowElem = document.querySelector(".row");
+const overlayElem = document.querySelector(".overlay");
+const closeBtn = document.querySelector(".close-btn");
+const overlayImg = document.querySelector(".overlay-img");
 let dataArray = [];
+
 // FUNZIONI
+// Funzione che stampa Post
 const printPost = () => {
     dataArray.forEach((curItem) => {
         rowElem.innerHTML += `
             <div class="col">
-                <div class="photo-polaroid">
+                <div class="photo-polaroid" data-post-id="${curItem.id}">
                     <div class="photo">
-                        <img src="${curItem.thumbnailUrl}">
+                        <img src="${curItem.url}">
                     </div>
                     <div class="caption">${curItem.title}</div>
                 </div>
             </div>`
     })
 }
+
+// Funzione che mostra Overlay
+const showOverlay = (clickElem) => {
+    const clickedPostId = clickElem.dataset.postId;
+    const imgToShowElem = dataArray.find((curItem) => curItem.id == clickedPostId);
+    overlayImg.innerHTML = `<img src="${imgToShowElem.url}">`
+    overlayElem.classList.remove("d-none");
+    overlayElem.classList.add("d-flex");
+}
+
+const hideOverlay = () => {
+    overlayElem.classList.remove("d-flex");
+    overlayElem.classList.add("d-none");
+}
+
+// Funzione che crea Event Listener su post
+const addEvent = () => {
+    const posts = document.querySelectorAll(".photo-polaroid");
+    posts.forEach((curPost) => {
+        curPost.addEventListener("click", () => showOverlay(curPost));
+    });
+};
+
 // ESECUZIONE LOGICA
-axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6", {timeout: 5000}).then((resp) => {
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6", { timeout: 6000 }).then((resp) => {
     dataArray = resp.data;
     console.log(dataArray);
     printPost()
+    addEvent()
 })
+
+closeBtn.addEventListener("click", hideOverlay);
+
